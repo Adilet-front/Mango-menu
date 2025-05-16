4d2d9d5aa27d01d5c8c26ebe6f31254bfedf4603
Adilet333 <adilettinadilettin@gmail.com>
2025-05-17 01:39:21 +0600

    pages

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <header>
+      <div className="flex justify-center w-100%">
+        <div className=" w-240 h-200 bg-amber-500 flex justify-center">
+          <Link className=" text-9xl cursor-pointer" to="/menu">
+            Menu
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
